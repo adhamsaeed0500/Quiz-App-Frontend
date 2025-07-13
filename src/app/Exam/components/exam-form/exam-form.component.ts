6059cbf34c7b6c1c1fb54748e27638c17f33ea91
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-07-13 13:43:38 +0300

    feat(ExamForm) : add ExamForm Component For Adding and Updating Exam

---
+++ src/app/Exam/components/exam-form/exam-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './exam-form.component.html',
+  styleUrl: './exam-form.component.css'
+})
+export class ExamFormComponent {
+
+}
